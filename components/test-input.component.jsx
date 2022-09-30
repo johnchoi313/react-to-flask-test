8c39712f25340f6a8c146476d27676ca5fcb4421
Olivia Lynn <olynn@andrew.cmu.edu>
2022-09-30 15:31:40 -0400

    Revert "Revert "about to merge""
    This reverts commit ae5fdd291389ee2ca07ae5ed818c64cbbd2e5eef.

---
+++ components/test-input.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class TestInput extends Component {
+  render() {
+    return <div>Hello im the test input component</div>;
+  }
+}
+
+export default TestInput;
