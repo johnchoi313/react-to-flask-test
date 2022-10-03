487ac8512e412aa3dc5ce88867101bdb7bd24572
John Choi <johnchoi313@gmail.com>
2022-10-03 02:51:38 -0400

    pushed sh

---
+++ main-shell-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+x-terminal-emulator -e npm --prefix /home/ubuntu/Desktop/react-to-flask-test-main run dev
+x-terminal-emulator -e python3 /home/ubuntu/Desktop/react-to-flask-test-main/routes.py
