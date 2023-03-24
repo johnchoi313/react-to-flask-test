e2ff7c65d1d95575bd7a2659429902b4ccf4afd5
John Choi <johnchoi313@gmail.com>
2023-03-24 02:07:40 -0400

    boolean gripper value

--- routes.py
+++ routes.py
@@ -160,13 +160,13 @@ def send_angles_sequence():
             
             #newGripperValue = int(translate(angle_gripper, -105, 105, 0, 255))
             #mc.set_gripper_value(newGripperValue, 70)
+            #print(f"Sending gripper value: " + str(newGripperValue) + " from input: " + str(angle_gripper))
             
             if(angle_gripper >= 0):
                 mc.set_gripper_state(1, 70)
             else:
                 mc.set_gripper_state(0, 70)
             
-            print(f"Sending gripper value: " + str(newGripperValue) + " from input: " + str(angle_gripper))
             
             time.sleep(1)
 
